c9253c773f1fb820aad5539f1cbb97a7936a8e4d
Vladislav <vladan982004@gmail.com>
2019-02-11 10:52:13 +0500

    Start module delete

--- www/modules/contacts/delete-message.php
+++ www/modules/contacts/delete-message.php
@@ -1,14 +1,31 @@
 <?php 
 
-$title = "Сообщения";
+//Закрываем доступ к странице для не админа
+if (!isAdmin()) {
+	header("Location: " . HOST);
+	die();
+}
 
-$messages = R::find('messages', 'ORDER BY id DESC');
+$title = "Удалить сообщение";
+
+//Из таблицы messages загружаем сообщение по id
+$message = R::load('messages', $_GET['id']);
+
+if (isset($_POST['messageDelete'])) {
+
+	
+	
+	R::trash($cat);
+	header("Location: " . HOST . "blog/categories?result=catDeleted");
+	exit();
+
+}
 
 //Готовим контент для центральной части
 //ob_start() - буферизированный вывод.
 ob_start();
 include ROOT . "templates/_parts/_header.tpl";
-include ROOT . "templates/contacts/messages.tpl";
+include ROOT . "templates/contacts/delete-message.tpl";
 //ob_get_contents() - получает контент записанный между функциями ob_start и ob_get_contans из буфера.
 $content = ob_get_contents();
 ob_end_clean();
---
+++ www/templates/contacts/delete-message.tpl
@@ -0,0 +1,15 @@
+<div class="sticky-footer-content">
+	<div class="container user-content pt-80">
+		<div class="row">
+			<div class="col-10 offset-1">
+				<div class="title-1 post-add__title">Удалить сообщение</div>
+				<p>Вы действительно хотите удалить сообщение ?</p>
+				<form class="post-add-form" action="<?=HOST?>delete-message?id=<?=$cat['id']?>" method="POST">
+					<div class="post-add-form-button"><input class="button button-delete" type="submit" value="Удалить" name="messageDelete" />
+						<div class="post-add-form-button__cancel"><a class="button" href="<?=HOST?>messages">Отмена</a></div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
